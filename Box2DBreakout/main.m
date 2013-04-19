4382d54afef6ddb231357e9378ac0f57ef5b0355
Sam Lee <SamLee@dn-wlan-181-239.sns.otago.ac.nz>
2013-04-19 17:56:33 +1200

    Initial Commit

---
+++ Box2DBreakout/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Box2DBreakout
+//
+//  Created by Sam Lee on 2013/04/19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
